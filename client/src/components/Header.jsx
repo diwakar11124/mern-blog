b38d4fea741fc1b306327901e8c1f9178b86038b
diwakar11124 <diwakar11124@gmail.com>
2024-04-09 21:42:46 +0530

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,38 +1,43 @@
-import React from 'react'
-import {Button, Navbar, TextInput} from 'flowbite-react';
-import { Link , useLocation} from 'react-router-dom';
+
+import {Avatar, Dropdown, Button, Navbar, TextInput} from 'flowbite-react';
+import { Link , useLocation ,useNavigate} from 'react-router-dom';
 import {AiOutlineSearch} from 'react-icons/ai';
-import {FaMoon} from 'react-icons/fa';
+import {FaMoon, FaSun} from 'react-icons/fa';
 
 export default function Header() {
     const path=useLocation().pathname;
   return (
     <Navbar className='border-b-2'>
-        <Link to="/" className='self-center whitespace-nowrap text-sm  sm:text-xl font-semibold dark:text-white'>
-           <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Diwakar's </span>
+        <Link 
+        to='/'
+        className='self-center whitespace-nowrap text-sm  sm:text-xl font-semibold dark:text-white'
+        >
+           <span className='px-2 py-1 bg-gradient-to-r from-cyan-500 via-green-300 to-green-600 rounded-lg text-white'>
+            One-Off
+            </span>
             Blog
         </Link>
         <form>
             <TextInput
             type='text'
-            placeholder='Search......'
-            rightIcon={AiOutlineSearch}
+            placeholder='Search...'
+            
             className='hidden lg:inline'
             />
         </form>
         <Button className='w-16 h-10 lg:hidden' color='gray' rounded-lg>
         <AiOutlineSearch />
         </Button>
-        <div className="flex gap-6 md:order-2">
-            <Button className='w-10 h-10 hidden sm:inline' color='gray' rounded-lg>
-                <FaMoon />
+        <div className="flex gap-10 md:order-2">
+            <Button className='w-8 h-9 hidden sm:inline' color='gray' rounded-lg>
+                <FaMoon className='w-7 h-5'/>
             </Button>
             <Link to='/sign-in'>
-                <Button type="button" class="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                <Button gradientDuoTone='purpleToBlue' outline>
                     Sign In
                 </Button>
             </Link>
-            <Navbar.Toggle />
+            <Navbar.Toggle/>
         </div>
         <Navbar.Collapse>
                 <Navbar.Link active={path ==="/"} as={'div'}>

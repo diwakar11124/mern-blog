6f22f99a58f7577988d178b74f521049e5435373
diwakar11124 <diwakar11124@gmail.com>
2024-04-10 14:32:16 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 mongoose.connect(
     process.env.MONGO
 ).then(
-() => { console.log('MongoDb is connected');
+() => { console.log('MongoDb is connected!');
 }).
 catch((err) => {
     console.log(err);
